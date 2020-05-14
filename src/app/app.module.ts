398c75c2abc8185c373f9369c4ccc8f72e9f8e6d
nimash <nimashdilanka.13@cse.mrt.ac.lk>
2020-05-14 21:55:58 +1000

    C1: Tutorial with https://angular.io/tutorial/toh-pt4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
-    ])
+    FormsModule,
   ],
   declarations: [
     AppComponent,
-    TopBarComponent,
-    ProductListComponent,
-    ProductAlertsComponent
+    HeroesComponent,
+    HeroDetailComponent,
   ],
   bootstrap: [ AppComponent ]
 })
