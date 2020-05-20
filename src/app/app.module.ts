10635687d0f94a6a190e8e79565986e4b7507945
nimash <nimashdilanka.13@cse.mrt.ac.lk>
2020-05-20 13:35:49 +1000

    Revert "C1: Tutorial with https://angular.io/tutorial/toh-pt4"
    This reverts commit 398c75c2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+    ])
   ],
   declarations: [
     AppComponent,
-    HeroesComponent,
-    HeroDetailComponent,
+    TopBarComponent,
+    ProductListComponent,
+    ProductAlertsComponent
   ],
   bootstrap: [ AppComponent ]
 })
