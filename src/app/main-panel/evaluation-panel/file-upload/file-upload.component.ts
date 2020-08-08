cfbabf0bf909d707f656f651eb65bf2a3aeeb39b
nimash <nimashdilanka.13@cse.mrt.ac.lk>
2020-08-08 20:17:25 +1000

    w9: initiate file uploading process

---
+++ src/app/main-panel/evaluation-panel/file-upload/file-upload.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-file-upload',
+  templateUrl: './file-upload.component.html',
+  styleUrls: ['./file-upload.component.css']
+})
+export class FileUploadComponent {
+
+  constructor() { }
+
+}
